633c3e8ef55c09c31232cce54a4e79a3d0b04850
Nathaly Toledo <nathaly12toledo@gmail.com>
2023-01-12 01:22:22 -0400

    Update 200-number-of-islands.py

--- easy/200-number-of-islands.py
+++ easy/200-number-of-islands.py
@@ -14,13 +14,13 @@ def numIslands(grid: List[List[str]]) -> int:
         # check if current cell is a land
         if grid[er][ec] == 1:
             # iterate over array to check for position having a land (1)
-            stack = [next]
+            stack = set(next)
             while stack:
                 curr = stack.pop()
                 sr, sc = curr
 
                 # mark as 2
-                grid[sr, sc]
+                grid[sr, sc] = 2
                 # handle children
                 left = grid[sr][sc - 1] if sc - 1 >= 0 else None
                 right = grid[sr][sc + 1] if sc + 1 < len(grid[sr]) else None
@@ -29,13 +29,17 @@ def numIslands(grid: List[List[str]]) -> int:
 
                 # check if adjacent pixels are present (top, bottom, left, right)
                 if top == 1:
-                    stack.append((sr - 1, sc))
+                    stack.add((sr - 1, sc))
                 if bottom == 1:
-                    stack.append((sr + 1, sc))
+                    stack.add((sr + 1, sc))
                 if left == 1:
-                    stack.append((sr, sc - 1))
+                    stack.add((sr, sc - 1))
                 if right == 1:
-                    stack.append((sr, sc + 1))
+                    stack.add((sr, sc + 1))
+            # when we get to the second loop, redefine er and ec
+            # TODO: add the next number (column) because we know is likely empty, else next row
+            ec = sc + 1 if sc+1 <  len(grid) else 0 # else first column
+            er = er+1 if ec == 0 else er # else keep it simple because it is the same row but different column
 
                 # redefine ec or er if necessary (not last items for example)
             # count island
@@ -49,3 +53,13 @@ def numIslands(grid: List[List[str]]) -> int:
             nexti = (er + 1, 0)
         else:
             nexti = None
+
+
+if __name__ == '__main__':
+
+    test = set()
+    test.add((2,2))
+    test.add((2, 2))
+    test.add((2, 2))
+    test.add((2, 1))
+    print(test)
