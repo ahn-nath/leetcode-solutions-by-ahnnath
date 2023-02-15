0505160ab8efe957caf41cc58406d61abb7a63b3
Nathaly Toledo <nathaly12toledo@gmail.com>
2023-02-15 19:55:03 -0400

    Create 692-top-k-frequent-words.py

---
+++ medium/692-top-k-frequent-words.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+def topKFrequent(words: List[str], k: int) -> List[str]:
+    pass
+
+
+if __name__ == '__main__':
+    words = [1, 10, 2]
+    k = 2
+
+    print(words.sort())
+
+    for i in words:
+        print(i)
+
