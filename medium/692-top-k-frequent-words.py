e55aedc18dc804ed86cd59e8f26c729e5ba54e09
Nathaly Toledo <nathaly12toledo@gmail.com>
2023-02-15 20:06:43 -0400

    Update 692-top-k-frequent-words.py
    Updated code with comments detailing the logic to be added and tested.

--- medium/692-top-k-frequent-words.py
+++ medium/692-top-k-frequent-words.py
@@ -2,15 +2,35 @@ from typing import List
 
 
 def topKFrequent(words: List[str], k: int) -> List[str]:
-    pass
+    # sort the words in lexicographical order
+    words.sort()
+
+    # for loop to keep unique keys
+    keys = []
+    count = []
+    for w in words:
+        # for each new key, record and set space for its count
+        if w not in keys:
+            words.append(w)
+            count.append(0)
+        else:
+            # get the index and update the count
+            i = keys.index(w)
+            count[i] = count[i] + 1
+
+    # for loop to get the index of the max value until k is reached
+
+    # sort the list of indexes and iterate to get them in a different list
 
 
 if __name__ == '__main__':
-    words = [1, 10, 2]
+    words = ["i", "love", "leetcode", "i", "love", "coding"]
     k = 2
 
-    print(words.sort())
+    words.sort()
+    print(words)
 
-    for i in words:
-        print(i)
+    seti = set(words)
+    print(seti)
 
+    # list comprehension to cle
