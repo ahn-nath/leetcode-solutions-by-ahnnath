e75f2ccd9c78581d5b09b0163501f1e960ba0616
Nathaly Toledo <nathaly12toledo@gmail.com>
2023-04-30 16:45:44 -0400

    Create 110-balanced-binary-tree.py

---
+++ easy/110-balanced-binary-tree.py
@@ -0,0 +1,33 @@
+from idlelib.tree import TreeNode
+from typing import Optional
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def dfs(root):
+            if not root:
+                return [True, 0]
+
+            left = dfs(root.left)
+            right = dfs(root.right)
+            balanced = (left[0] and right[0] and abs(left[1] - right[1]) <= 1)
+
+            return [balanced, max(left[1], right[1]) + 1]
+
+        return dfs(root)[0]
+
+
+if __name__ == '__main__':
+    # example 1
+    root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
+    print(Solution().isBalanced(root))
