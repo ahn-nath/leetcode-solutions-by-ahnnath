266c547a8edb6784677fcf271359fe1882adb2d1
Nathaly Toledo <nathaly12toledo@gmail.com>
2023-05-16 13:09:53 -0400

    Refactored: added solution that would work

--- medium/148-sort-list.py
+++ medium/148-sort-list.py
@@ -14,82 +14,39 @@ class ListNode:
         return f"{self.val} -> {self.next}"
 
 
-def sortList_slow(head: Optional[ListNode]) -> Optional[ListNode]:
+def sortList(head: Optional[ListNode]) -> Optional[ListNode]:
     if not head or not head.next:
         return head
 
     # iterate over list
-    is_not_checked_as_sorted = True  # if the list has not been verified as sorted
-    while is_not_checked_as_sorted:
-        # check the whole list to see if sorted, we assume it is sorted at first
-        is_not_checked_as_sorted = False
+    is_unsorted = False
+    was_iterated = False
+    first_item = None
+    while is_unsorted or not was_iterated:
+        # check the whole list to see in unsorted
+        is_unsorted = False
         node = head
-        prev = None
         nexti = node.next
         # iterate over the whole list to swap elements
         while nexti:
             # swap and mark list as unsorted
             if node.val > nexti.val:
-                is_not_checked_as_sorted = True
-
-                # if the node of head will be swapped, update the head reference
-                if node is head:
-                    head = nexti
-
+                is_unsorted = True
                 # swap elements: the nexti node should be before the node and point to node.next for its next attribute,
                 # and the nexti node should point to the nexti.next value
                 nexti.next, node.next = node, nexti.next
-                if prev:
-                    prev.next = nexti
-                # update values
-                node = nexti
 
-            # check next
-            prev = node
-            node = node.next
+            # update values
+            node = nexti
             nexti = node.next
+        if is_unsorted and not first_item:
+            first_item = node
+            was_iterated = True
 
     print(head)
-    return head
-
-
-def sortList(head: Optional[ListNode]) -> Optional[ListNode]:
-    if not head or not head.next:
-        return head
-
-    # iterate over strong to get elements
-    node = head
-    numbers = []
-    while node:
-        numbers.append(node.val)
-        node = node.next
-
-    # sort results and compare contents
-    if numbers == numbers.sort():
-        return head
-
-    # construct linked list
-    head = ListNode(numbers[0])
-    prev = head
-    if len(numbers) > 1:
-        for num in numbers[1:]:
-            # create new node and add to linked list
-            new_node = ListNode(num)
-            prev.next = new_node
-            # update prev
-            prev = new_node
-    print(head)
-    return head
-
-
-
+    return first_item
 
 
 if __name__ == '__main__':
     linked_list = ListNode(4, ListNode(2, ListNode(1, ListNode(3))))
-    # [-1,5,3,4,0]
-    linked_list2 = ListNode(-1, ListNode(5, ListNode(3, ListNode(4, ListNode(0)))))
-
-    print(sortList(linked_list))
-    print(sortList(head=None))
-    print(sortList(linked_list2))  # [-1,0,3,4,5]
+    sortList(linked_list)
--- medium/994-rotting-oranges.py
+++ medium/994-rotting-oranges.py
@@ -1,136 +1,99 @@
 from typing import List
 
-
 def orangesRotting(grid: List[List[int]]) -> int:
-    minutes_count = 0
+    """
+     This function takes in a grid of 0s, 1s, and 2s. 0s are empty cells, 1s are fresh oranges, and 2s are rotten oranges.
+        The function returns the number of minutes it takes for all oranges to rot. If it is impossible for all oranges to rot, the function returns -1.
 
-    # find all rotten oranges
-    for row in range(len(grid)):
-        for column in range(len(grid[0])):
-            cell = grid[row][column]
-            # if the current cell is healthy or empty, skip
-            if 1 == cell or 0 == cell:
-                continue
-            # else, if current cell is healthy (1), check if the adjacent cells are rotten
-            else:
-                # check the top
-                top = grid[row - 1][column] if row > 0 else 0
-                # check the bottom
-                bottom = grid[row + 1][column] if row < (len(grid) - 1) else 0
-                # check the left
-                left = grid[row][column - 1] if column > 0 else 0
-                # check the right
-                right = grid[row][column + 1] if column < (len(grid[0]) - 1) else 0
+        @param grid: a grid of 0s, 1s, and 2s
+        @return: the number of minutes it takes for all oranges to rot
 
-                # if any of the adjacent cells are rotten, rot the current cell
-                if top == 1 or bottom == 1 or left == 1 or right == 1:
-                    # mark the current orange as rotten
-                    # grid[row][column] = 2
-
-                    if top == 1:
-                        # mark top as rotten
-                        grid[row - 1][column] = 2
-                    if bottom == 1:
-                        # mark bottom as rotten
-                        grid[row + 1][column] = 2
-                    if left == 1:
-                        # mark left as rotten
-                        grid[row][column - 1] = 2
-                    if right == 1:
-                        # mark right as rotten
-                        grid[row][column + 1] = 2
-
-                    # increment count
-                    minutes_count += 1
-
-    print(grid)
-
-    for row in grid:
-        # if any in current group is 1 after this row iteration, return -1
-        if 1 in row:
-            print(-1)
-            print(grid)
-            return -1
-
-    print(minutes_count)
-    print(grid)
-    return minutes_count
-
-
-def orangesRotting(grid: List[List[int]]) -> int:
-    print('\n')
-
+    """
     minutes_count = 0
     # initialize with the first element in grid
-    oranges_stack = [(0, 0)]
+    current_rotten_oranges_stack = []
+    next_rotten_oranges_stack = []
 
-    # TODO: change so that it would iterate through all rotten oranges and check all cases
-    # find all rotten oranges
-    while oranges_stack:
-        ele = oranges_stack.pop()
+    healthy_oranges_count = 0
+
+    # initialize healthy_oranges_count and current_rotten_oranges_stack
+    for row in range(len(grid)):
+        for column in range(len(grid[0])):
+            # count the number os healthy oranges
+            if 1 == grid[row][column]:
+                healthy_oranges_count += 1
+            # track current rotten oranges
+            if 2 == grid[row][column]:
+                current_rotten_oranges_stack.append((row, column))
+
+    # if there are no healthy oranges, return 0, because no minutes are needed
+    if 0 == healthy_oranges_count:
+        return 0
+
+    # while there are healthy oranges and there are rotten oranges
+    while current_rotten_oranges_stack and healthy_oranges_count > 0:
+        # get current element
+        ele = current_rotten_oranges_stack.pop()
         row = ele[0]
         column = ele[1]
 
-        # if the current cell is healthy or empty, skip
-        if 0 == grid[row][column]:
-            continue
-        # else, if current cell is healthy (1), check if the adjacent cells are rotten
-        else:
-            # check the top
-            top = grid[row - 1][column] if row > 0 else 0
-            # check the bottom
-            bottom = grid[row + 1][column] if row < (len(grid) - 1) else 0
-            # check the left
-            left = grid[row][column - 1] if column > 0 else 0
-            # check the right
-            right = grid[row][column + 1] if column < (len(grid[0]) - 1) else 0
+        # check the top
+        top = grid[row - 1][column] if row > 0 else 0
+        # check the bottom
+        bottom = grid[row + 1][column] if row < (len(grid) - 1) else 0
+        # check the left
+        left = grid[row][column - 1] if column > 0 else 0
+        # check the right
+        right = grid[row][column + 1] if column < (len(grid[0]) - 1) else 0
 
-            # if any of the adjacent cells are healthy, rot them and add to oranges_stack
-            if grid[row][column] == 2:
-                if top == 1 or bottom == 1 or left == 1 or right == 1:
-                    # mark the current orange as rotten
-                    # grid[row][column] = 2
+        # if any of the adjacent cells are healthy, rot them and add to oranges_stack, to see future oranges to rot
+        if top == 1 or bottom == 1 or left == 1 or right == 1:
 
-                    if top == 1:
-                        # mark top as rotten
-                        grid[row - 1][column] = 2
-                        oranges_stack.append((row - 1, column))
-                    if bottom == 1:
-                        # mark bottom as rotten
-                        grid[row + 1][column] = 2
-                        oranges_stack.append((row + 1, column))
-                    if left == 1:
-                        # mark left as rotten
-                        grid[row][column - 1] = 2
-                        oranges_stack.append((row, column - 1))
-                    if right == 1:
-                        # mark right as rotten
-                        grid[row][column + 1] = 2
-                        oranges_stack.append((row, column + 1))
-                    # increment count
-                    minutes_count += 1
+            if top == 1:
+                # mark top as rotten
+                grid[row - 1][column] = 2
+                healthy_oranges_count -= 1
+                # add to next_rotten_oranges_stack
+                next_rotten_oranges_stack.append((row - 1, column))
 
-            # set current as rotten if is healthy and if any adjacent is rotten
-            if grid[row][column] == 1:
-                if top == 0 and bottom == 0 and left == 0 and right == 0:
-                    return -1
+            if bottom == 1:
+                # mark bottom as rotten
+                grid[row + 1][column] = 2
+                healthy_oranges_count -= 1
+                # add to next_rotten_oranges_stack
+                next_rotten_oranges_stack.append((row + 1, column))
 
-                if top == 2 or bottom == 2 or left == 2 or right == 2:
-                    grid[row][column] = 2
-                    # increment count
-                    minutes_count += 1
+            if left == 1:
+                # mark left as rotten
+                grid[row][column - 1] = 2
+                healthy_oranges_count -= 1
+                # add to next_rotten_oranges_stack
+                next_rotten_oranges_stack.append((row, column - 1))
 
-    print("grid is", grid)
+            if right == 1:
+                # mark right as rotten
+                grid[row][column + 1] = 2
+                healthy_oranges_count -= 1
+                # add to next_rotten_oranges_stack
+                next_rotten_oranges_stack.append((row, column + 1))
 
-    for row in grid:
-        # if any in current group is 1 after this row iteration, return -1
-        if 1 in row:
-            print(-1)
-            print(grid)
-            return -1
+        # if we have reached the goal, set current rotten oranges as empty
+        if healthy_oranges_count <= 0:
+            current_rotten_oranges_stack = []
 
-    print(minutes_count)
-    print(grid)
+        # if pair_index is greater than the length of oranges_stack, reset it to 0 if there are still healthy oranges
+        if len(current_rotten_oranges_stack) <= 0:
+            # increment count
+            minutes_count += 1
+            # set current to next
+            current_rotten_oranges_stack = next_rotten_oranges_stack
+            next_rotten_oranges_stack = []
+
+    # if there are still healthy oranges, return -1
+    if healthy_oranges_count > 0:
+        return -1
+
+    # out
     return minutes_count
 
 
@@ -138,6 +101,9 @@ if __name__ == '__main__':
     orangesRotting(grid=[[2, 1, 1], [1, 1, 0], [0, 1, 1]])  # 4
 
     orangesRotting(grid=[[2, 1, 1], [0, 1, 1], [1, 0, 1]])  # -1
+
+    orangesRotting(grid=
+                   [[2, 1, 1], [1, 1, 1], [0, 1, 2]])  # 2
 
     orangesRotting(grid=[[0, 2]])  # 0
 
@@ -150,3 +116,5 @@ if __name__ == '__main__':
                     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])  # 58
 
     orangesRotting([[2, 1, 1], [1, 1, 1], [0, 1, 2]])  # 2
+
+    orangesRotting(grid=[[1, 1, 2, 0, 2, 0]])  # 2
